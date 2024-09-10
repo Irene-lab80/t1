25d13915a3c5aa0d678fe34a4c9ba28c56cb871c
Irina <ivzhyurkevich@gmail.com>
2024-09-10 22:58:30 +0300

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Home } from "./pages";
 
-createRoot(document.getElementById('root')!).render(
+export enum ROUTES {
+  HOME = "/",
+  CART = "/cart",
+  PRODUCT = "/product",
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <header>header</header>
+        <Outlet />
+        <footer>footer</footer>
+      </div>
+    ),
+    errorElement: <div>error</div>,
+    children: [
+      {
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: ROUTES.CART,
+        element: <div>cart</div>,
+      },
+      {
+        path: `${ROUTES.PRODUCT}/:id`,
+        element: <div>product</div>,
+        index: true,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/Home/ui/Home.tsx
@@ -0,0 +1,5 @@
+import style from "./Home.module.css";
+
+export const Home = (): JSX.Element => {
+  return <div className={style.wrapper}>Home</div>;
+};
