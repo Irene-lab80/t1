968bd4c72370bdc7d1d42e48e6c3819a608c90a3
Irina <ivzhyurkevich@gmail.com>
2024-09-15 01:41:55 +0300

    fix: fix merge conflict

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,7 +1,5 @@
 export { CartIcon } from "./CartIcon";
 export { CrossIcon } from "./CrossIcon";
-export { MinusIcon } from "./MinusIcon";
-export { PlusIcon } from "./PlusIcon";
 export { StarIcon } from "./StarIcon";
 export { MinusIcon } from "./MinusIcon";
 export { PlusIcon } from "./PlusIcon";
